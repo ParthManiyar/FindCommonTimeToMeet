8253f7577d597a17cc634128c3979738efc9895b
Parth <parth.maniyar@akridata.com>
2021-08-13 21:32:51 +0530

    Initial commit with Database Model for User and UserTimingPreferences

---
+++ findCommonTimeToMeetApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FindcommontimetomeetappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'findCommonTimeToMeetApp'
---
+++ findCommonTimeToMeetApp/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+import uuid
+from django.core.exceptions import ValidationError
+import pytz
+
+
+def validate_timezone(value):
+    if pytz.all_timezones in value:
+        return value
+    else:
+        raise ValidationError("This field accepts only valid time zone")
+
+
+class TimestampModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class UserTimingPreferences:
+    day_start_time = models.DateTimeField()
+    day_end_time = models.DateTimeField()
+    time_zone = models.CharField(validators=[validate_timezone])
+
+
+class User(TimestampModel):
+    id = models.UUIDField(primary_key=True,
+                          default=uuid.uuid4,
+                          editable=False)
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, unique=True)
+    password = models.CharField('password', max_length=128, null=True)
+    timingPreferences = models.ForeignKey(UserTimingPreferences,
+                                          on_delete=models.CASCADE,
+                                          null=True)
